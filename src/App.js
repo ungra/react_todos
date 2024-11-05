992b9060fe3b7c8f588f44ea727314996e673ca7
junhee_lee2 <junhee_lee2@tmax.co.kr>
2024-11-05 11:29:13 +0900

    Initialize & make a todos app base(todo, todos)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setTodos((current) => [todo, ...current]);
+    setTodo("");
+  };
+  console.log(todos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className={styles.title}>My ToDos : ({todos.length}) </h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          value={todo}
+          placeholder="Write a ToDo"
+        ></input>
+      </form>
+      <Button text={"Submit"} />
     </div>
   );
 }
